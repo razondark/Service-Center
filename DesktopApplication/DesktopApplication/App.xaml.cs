1fec03fcee3d453f57c06c2b70425f68767c647b
razondark <bryakovdima@yandex.ru>
2024-03-27 16:07:30 +0300

    recreate project

--- DesktopApplication/DesktopApplication/App.xaml.cs
+++ DesktopApplication/DesktopApplication/App.xaml.cs
@@ -2,7 +2,7 @@
 using System.Data;
 using System.Windows;
 
-namespace CourseProject
+namespace DesktopApplication
 {
 	/// <summary>
 	/// Interaction logic for App.xaml
--- DesktopApplication/DesktopApplication/MainWindow.xaml.cs
+++ DesktopApplication/DesktopApplication/MainWindow.xaml.cs
@@ -9,8 +9,11 @@ using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
 
-namespace CourseProject
+namespace DesktopApplication
 {
+	/// <summary>
+	/// Interaction logic for MainWindow.xaml
+	/// </summary>
 	public partial class MainWindow : Window
 	{
 		public MainWindow()
