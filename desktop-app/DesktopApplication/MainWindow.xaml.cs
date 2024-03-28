134b692bbfa0b8b23dc1f1e60fc427f0dd49aa50
razondark <bryakovdima@yandex.ru>
2024-03-28 10:31:45 +0300

    add wpf form

--- desktop-app/DesktopApplication/MainWindow.xaml.cs
+++ desktop-app/DesktopApplication/MainWindow.xaml.cs
@@ -11,14 +11,43 @@ using System.Windows.Shapes;
 
 namespace DesktopApplication
 {
-	/// <summary>
-	/// Interaction logic for MainWindow.xaml
-	/// </summary>
 	public partial class MainWindow : Window
 	{
 		public MainWindow()
 		{
 			InitializeComponent();
 		}
+
+		private void Border_MouseDown(object sender, MouseButtonEventArgs e)
+		{
+			if (e.ChangedButton == MouseButton.Left)
+			{
+				this.DragMove();
+			}
+		}
+
+		private bool IsMaximized = false;
+		private void Border_MouseLeftButtonDown(object sender, MouseButtonEventArgs e)
+		{
+			if (e.ClickCount == 2)
+			{
+				if (IsMaximized) 
+				{
+					this.WindowState = WindowState.Normal;
+					this.Width = 1080;
+					this.Height = 720;
+
+					IsMaximized = false;
+				}
+				else
+				{
+					this.WindowState = WindowState.Maximized;
+
+					IsMaximized = true;
+				}
+			}
+		}
+
+
 	}
 }
